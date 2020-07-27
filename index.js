ac3a27c29d6ae8f076ab2b2c8255e3bc10b85afb
KendrewC <kendrewchanys@gmail.com>
2020-07-28 00:03:50 +0800

    telegram commands

--- index.js
+++ index.js
@@ -13,8 +13,48 @@ const sendMessage = (userid, message) => {
 		.catch(err => console.log(err));
 }
 
+const getUpdates = (callback) => {
+	axios.get(`${TELE_URL}/getUpdates`)
+		.then(res => {
+			const data = res.data.result;
+			data.forEach(msg => {
+				callback(null, msg);
+			})
+			const offset = data[data.length-1].update_id + 1
+			axios.get(`${TELE_URL}/getUpdates?offset=${offset}`)
+				.catch(err => console.log(err));
+			// This axios command is useless once we do webhooks
+		})
+		.catch(err => callback(err, null));
+}
+
+const checkCommands = (err, result) => {
+	if (err) {
+		console.log(err);
+	} else {
+		const message = result.message;
+		if (message.entities != undefined && message.entities[0].type === 'bot_command') {
+			// Check whether command exists && if it's a command
+			const command = message.text;
+			const chatID = message.chat.id;
+			switch (command) {
+				case "/help":
+					sendMessage(chatID, "You've just typed /help :D");
+					break;
+				default:
+					sendMessage(chatID, "Sorry! That's an invalid command :(");
+					break;
+			}
+		}
+	}
+}
+
+getUpdates(checkCommands);
+// Sends updates based upon commands by user
+
+
 // Get TelegramID: https://t.me/userinfobot
 const TELEGRAM_ID = 0 // Insert ur id here
 
-sendMessage(TELEGRAM_ID, "hi there");
+// sendMessage(TELEGRAM_ID, "hi there");
 
