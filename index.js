ef84d59943be6ac4f1f9cb92b421b75d50d23f33
KendrewC <kendrewchanys@gmail.com>
2020-07-27 23:36:57 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+const API_SECRET = "1034174341:AAEU_XhxGFzwTNpHEFJ8Y_R71aQ6BLJdEMw";
+
+const TELE_URL = `https://api.telegram.org/bot${API_SECRET}`;
+
+const sendMessage = (userid, message) => {
+	const data = {
+		chat_id: userid,
+		text: message
+	}
+	axios.post(`${TELE_URL}/sendMessage`, data)
+		.catch(err => console.log(err));
+}
+
