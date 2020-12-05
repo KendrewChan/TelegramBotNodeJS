6a5ca30adcc6c7fd1b2eac7f47a75f5269e300e4
KendrewC <kendrewchanys@gmail.com>
2020-12-06 00:49:21 +0800

    Add production mode

--- index.js
+++ index.js
@@ -2,31 +2,34 @@ require('dotenv').config();
 const Telegram = require("./telegram");
 var bodyParser = require('body-parser');
 var express = require('express');
+const { checkCommands } = require('./telegram');
 
 const app = express();
 
-// Bodyparser middleware 
-//   -> it passes through here before arriving as req (req.body)
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-app.use(bodyParser.json());
+const PRODUCTION = false;
 
-app.get('/', (req, res) => {
-	res.send("Webhooks on /webhooks!");
-})
+if (PRODUCTION) {
+	// Bodyparser middleware 
+	//   -> it passes through here before arriving as req (req.body)
+	app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+	);
+	app.use(bodyParser.json());
 
-app.post('/webhooks', (req, res) => {
-	const { message } = req.body;
-	Telegram.checkCommands(null, message);
-	// res.sendStatus(200);
-	res.send("webhooker");
-});
+	app.post('/', (req, res) => {
+		// Web hooks
+		const { message } = req.body;
+		Telegram.checkCommands(null, message);
+		// res.sendStatus(200);
+		res.send("webhooker");
+	});
 
-
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () =>
-  console.log(`Server up and running on port ${port} !`)
-);
+	const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+	app.listen(port, () =>
+	console.log(`Server up and running on port ${port} !`)
+	);
+} else {
+	Telegram.getUpdates(checkCommands);
+}
